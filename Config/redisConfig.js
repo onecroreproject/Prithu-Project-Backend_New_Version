ad92d02838fb6ae23f37c926310f6398e484e384
Balakrishnan P <bala2772003@gmail.com>
2025-10-11 21:09:20 +0530

    v1

--- Config/redisConfig.js
+++ Config/redisConfig.js
@@ -1,5 +1,10 @@
-module.exports = {
-  host: process.env.REDIS_HOST || "be-backend",
-  port: process.env.REDIS_PORT || 5000,
+const Redis = require("ioredis");
+
+// Use REDIS_URL if available (Render style), else fallback to local setup
+const redis = new Redis(process.env.REDIS_URL || {
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || 6379,
   retryStrategy: (times) => Math.min(times * 50, 2000),
-};
+});
+
+module.exports = redis;
