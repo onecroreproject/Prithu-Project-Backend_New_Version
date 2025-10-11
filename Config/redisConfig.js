4bc3986c303056c1f0c8478ec9e3acbd8ef88186
Balakrishnan P <bala2772003@gmail.com>
2025-10-11 20:05:53 +0530

    v1

--- Config/redisConfig.js
+++ Config/redisConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: process.env.REDIS_HOST || "redis",
-  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST || "be-backend",
+  port: process.env.REDIS_PORT || 5000,
   retryStrategy: (times) => Math.min(times * 50, 2000),
 };
