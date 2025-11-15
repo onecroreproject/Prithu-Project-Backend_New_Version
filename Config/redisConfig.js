bf9179ae90d4f6b575b7d9f417721614f5611787
Balakrishnan P <bala2772003@gmail.com>
2025-11-15 19:02:17 +0530

    15/11

--- Config/redisConfig.js
+++ Config/redisConfig.js
@@ -1,9 +1,12 @@
 const Redis = require("ioredis");
-const redis = new Redis({
+
+const redisClient = new Redis({
   host: process.env.REDIS_HOST || "127.0.0.1",
   port: process.env.REDIS_PORT || 6379,
   password: process.env.REDIS_PASSWORD || undefined,
 });
-redis.on("connect", () => console.log("✅ Redis connected successfully"));
-redis.on("error", (err) => console.error("❌ Redis connection error:", err));
- 
+
+redisClient.on("connect", () => console.log("✅ Redis connected successfully"));
+redisClient.on("error", (err) => console.error("❌ Redis connection error:", err));
+
+module.exports = redisClient;
---
+++ queue/hashTagTrendingQueue.js
@@ -0,0 +1,5 @@
+const createQueue = require("../queue");
+
+const hashtagTrendingQueue = createQueue("hashtag-trending");
+
+module.exports = hashtagTrendingQueue;
